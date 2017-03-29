a5bbbda4d52bd019987dbbcae019094b5650d28f
Regis Frey <RegisFrey@Darkfirestudios.com>
2017-03-28 21:51:21 -0700

    ace editor (via brace)

--- app/src/renderer/components/MainView/YarnEditor.vue
+++ app/src/renderer/components/MainView/YarnEditor.vue
@@ -2,16 +2,22 @@
   <div class="yarn-editor">
     <input placeholder="Title" />
     <input placeholder="Tags" />
-    <div class="yarn-editor-input ace-editor">
+    <div class="yarn-editor-input js-ace-editor" ref="editor">
     </div>
   </div>
 </template>
 
 <script>
+  import * as ace from 'brace';
+  import 'brace/mode/javascript';
+  import 'brace/theme/monokai';
 
   export default {
     data() {
-      return { };
+      return {
+        editor: null,
+        content: 'testing 1234',
+      };
     },
     components: {
 
@@ -20,10 +26,27 @@
 
     },
     methods: {
+      handleResize() {
 
+      },
+    },
+    mounted() {
+      // setup ace editor
+      this.editor = ace.edit(this.$refs.editor);
+      this.editor.getSession().setMode('ace/mode/javascript');
+      this.editor.setTheme('ace/theme/monokai');
+    },
+    ready() {
+      window.addEventListener('resize', this.handleResize);
+    },
+    beforeDestroy() {
+      window.removeEventListener('resize', this.handleResize);
     },
   };
 </script>
 
 <style>
+.yarn-editor-input{
+    min-height: 100px;
+}
 </style>
