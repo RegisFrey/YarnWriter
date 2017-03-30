b71c3124e2005d0c21dfd313d3643f16c8108a8e
Regis Frey <RegisFrey@Darkfirestudios.com>
2017-03-30 14:06:50 -0700

    working on editor (overlay container, font size, test soft wrap, mark feature todos, hide fake save status)

--- app/src/renderer/components/MainView/YarnEditor.vue
+++ app/src/renderer/components/MainView/YarnEditor.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="yarn-editor" v-if="showing">
+<div class="yarn-editor-container" v-if="showing">
+  <div class="yarn-editor">
     <header class="panel-header">
       <input class="yarn-editor-title" placeholder="Node Title" />
       <input class="yarn-editor-tags" placeholder="Add a Tag" />
@@ -16,13 +17,19 @@
       </button>
 
       <span class="yarn-editor-save-status">
-        Saving... <a href="">21 revisions</a>
+        <!-- Saving... <a href="">21 revisions</a> -->
       </span>
     </footer>
   </div>
+</div>
 </template>
 
 <script>
+  // TODO: clicking on container (but not editor window) closes window
+  // TODO: escape key closes window (i.e. showing = false)
+  // TODO: info button shows sidepanel with markup cheatsheet
+  // TODO: ROADMAP: google docs like versioning of document/nodes
+
   import '../YarnIcons/info';
 
   import * as ace from 'brace';
@@ -54,6 +61,12 @@
       this.editor = ace.edit(this.$refs.editor);
       this.editor.getSession().setMode('ace/mode/yarn');
       this.editor.setTheme('ace/theme/yarn');
+      this.editor.setOption('showPrintMargin', false);
+      this.editor.setOption('fontSize', '14pt');
+
+      // test softwrap
+      this.editor.getSession().setUseWrapMode(true);
+      this.editor.getSession().setWrapLimitRange();
     },
     ready() {
       window.addEventListener('resize', this.handleResize);
@@ -65,6 +78,16 @@
 </script>
 
 <style lang="scss">
+.yarn-editor-container {
+  display: block;
+  position: fixed;
+  left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(0.2,0,0.2,0.3);
+  z-index: 1000;
+}
 .yarn-editor{
   width: 90vw;
   height: 90vh;
@@ -73,20 +96,23 @@
   overflow: hidden;
   display:flex;
   flex-direction: column;
+  box-shadow: 0px 3px 8px rgba(0,0,0,0.2);
 }
 
 .panel-header{
   background-color: #EEF2F5;
   display:flex;
   flex-direction:column;
-  padding: 0 10px;
+  padding: 6px 10px;
   box-sizing: border-box;
+  flex: 0 0 auto;
 }
 .panel-footer{
   background-color: #EEF2F5;
   color: #A1AEC0;
   height: 32px;
   line-height: 32px;
+  flex: 0 0 auto;
   .action-btn {
     height: 32px;
     display: block;
@@ -101,8 +127,10 @@
 }
 
 .panel-content {
-  flex: 1;
+  flex: 1 1 auto;
   position: relative;
+  display:flex;
+  flex-direction:row;
 }
 
 .yarn-editor-save-status {
@@ -127,12 +155,15 @@
   flex:1;
 }
 .yarn-editor-input{
-  position: absolute; /* Added */
+  flex:1;
+  /*
+  position: absolute;
   top: 0;
   right: 0;
   bottom: 0;
   left: 0;
   height: 100%;
   width: 100%;
+  */
 }
 </style>
