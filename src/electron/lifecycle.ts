859968d52563bce1746b8fe5fac6e6a52dd96883
Regis Frey <RegisFrey@Darkfirestudios.com>
2020-04-07 20:32:06 -0700

    electron with some multi-window experimentation (CMD-N).

---
+++ src/electron/lifecycle.ts
@@ -0,0 +1,58 @@
+import { app, protocol, BrowserWindow, globalShortcut } from 'electron'
+import {
+  createProtocol,
+  installVueDevtools
+} from 'vue-cli-plugin-electron-builder/lib'
+import { openWindow } from './windows'
+import { WriterWindowType } from '@/types/Window'
+
+/* DUPLICATING */
+const isDevelopment = process.env.NODE_ENV !== 'production'
+// const hasLocalDevServer = !!process.env.WEBPACK_DEV_SERVER_URL
+
+/**
+ * The 'windows-all-closed' event is called, unsuprisingly, when the all the apps windows are closed.
+ */
+export function onWindowsAllClosed () {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+}
+
+/**
+ * The 'activate' event is called when the app is focused.
+ */
+export function onActivate () {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  // if (win === null) { createWindow() }
+}
+
+/**
+ * The 'ready' event  will be called when Electron has finished
+ * initialization and is ready to create browser windows.
+ * Some APIs can only be used after this event occurs.
+ */
+export async function onReady () {
+  if (isDevelopment && !process.env.IS_TEST) {
+    // Install Vue Devtools
+    // Devtools extensions are broken in Electron 6.0.0 and greater
+    // See https://github.com/nklayman/vue-cli-plugin-electron-builder/issues/378 for more info
+    // Electron will not launch with Devtools extensions installed on Windows 10 with dark mode
+    // If you are using Windows 10 dark mode, you may need to comment these lines to properly launch
+    // In addition, if the linked issue is closed, you should upgrade electron and remove this note
+    try {
+      await installVueDevtools()
+    } catch (e) {
+      console.error('Vue Devtools failed to install:', e.toString())
+    }
+  }
+  // register keys
+  globalShortcut.register('CommandOrControl+N', () => {
+    openWindow(WriterWindowType.Document)
+  });
+  // open a document
+  openWindow(WriterWindowType.Document)
+}
---
+++ src/electron/windows.ts
@@ -0,0 +1,70 @@
+import { BrowserWindow } from 'electron'
+
+export enum WriterWindowType {
+  Document = 'document',
+  Settings = 'settings',
+  Guide = 'guide'
+}
+
+/**
+ * Track currently open windows.
+ */
+export const windows: Array<BrowserWindow> = []
+
+/**
+ * Track the currently focused window.
+ */
+export const focusedWindow: BrowserWindow | null = null
+
+/**
+ * attach to BrowserWindow `beforeunload` event
+ */
+export function onWindowWillClose (event:{ preventDefault:Function, sender:BrowserWindow }) {
+  // console.log('onWindowWillClose', event)
+  // TODO: use the dialog API to ask if unsaved changes
+  // dialog.showMessageBox([browserWindow, ]options[, callback])
+}
+
+/**
+ * attach to BrowserWindow `close` event
+ */
+export function onWindowClosed (event:{ sender:BrowserWindow }) {
+  // console.log('onWindowWillClose', event)
+  // Stop tracking, remove from array
+  const index = windows.findIndex(w => w === event.sender)
+  windows.splice(index, 1)
+  console.log('tracked windows: ' + windows.length)
+  // TODO: if settings manager, unbind / reassign listeners
+}
+
+function setEntrypointForWindowType (window: BrowserWindow, type: WriterWindowType) {
+  if (process.env.WEBPACK_DEV_SERVER_URL) {
+    window.loadURL(process.env.WEBPACK_DEV_SERVER_URL as string)
+    if (!process.env.IS_TEST) window.webContents.openDevTools()
+  } else {
+    // createProtocol('app') // moved to initialization
+    window.loadURL('app://./index.html')
+  }
+  // document type uses the main entry point
+  // TODO: settings and guide have different entry points
+}
+
+export function openWindow (type: WriterWindowType) {
+  const window = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+  // Load an HTML Entry Point
+  setEntrypointForWindowType(window, type)
+  // Listen for Close
+  window.on('closed', onWindowClosed)
+  // Track
+  windows.push(window)
+}
+
+export function closeWindow (id: number) {
+  windows[id].close()
+}
