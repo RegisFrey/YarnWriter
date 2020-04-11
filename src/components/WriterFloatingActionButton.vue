37e2accd10ff72b209e9800499492292168a18e8
Regis Frey <RegisFrey@Darkfirestudios.com>
2020-04-10 17:01:02 -0700

    Update button to be in the style direction we are heading

---
+++ src/components/WriterFloatingActionButton.vue
@@ -0,0 +1,79 @@
+<template>
+  <button
+    class="wr-btn-fab"
+    v-on:click="$emit('click', $event)"
+    :disabled="disabled"
+    >
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+
+/**
+ * Floating Action Button for top level actions shown over the Workspace.
+ */
+export default {
+  props: {
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import '~./shared.scss';
+
+.wr-btn-fab {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+
+  @include floating;
+
+  border-radius: $size-control/2;
+  height: $size-control;
+  width: $size-control;
+  font-size: $size-control-font;
+  color: $color-control-text-light;
+  padding: 4px;
+  margin: 8px 4px;
+
+  border: 2px solid transparent;
+  &:focus {
+    border: 2px solid blue;
+    outline: none;
+  }
+  svg {
+    display: block; // icons should position correctly, otherwise they are inline
+  }
+}
+</style>
+
+<docs>
+```js
+<template>
+<WriterThemeContext show-toggle>
+  <WriterPegboard style="padding:20px;">
+    <WriterFloatingActionButton><IconMenu title="Menu" /></WriterFloatingActionButton>
+    <WriterFloatingActionButton><IconBook title="Guide" /></WriterFloatingActionButton>
+  </WriterPegboard>
+</WriterThemeContext>
+</template>
+
+<script>
+import IconMenu from 'vue-material-design-icons/Menu.vue'
+import IconBook from 'vue-material-design-icons/BookOpen.vue'
+
+export default {
+  components: {
+    IconMenu,
+    IconBook
+  }
+}
+</script>
+```
+</docs>
